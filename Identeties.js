88170d37a2f4bb55636d6f3722faaa171a125113
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2020-07-07 11:42:55 +0100

    fixed DID and added some comments

--- Identeties.js
+++ Identeties.js
@@ -3,20 +3,26 @@ const readline = require("readline")
 const store = require('store')
 
 module.exports = {
-    createIdentety: function (rl, storageLocation) {
-        rl.question('Enter name and Mnemonic?(leave empty for auto generation)', (answer) => {
+    createIdentety: function (rl, storageLocation) { // create a new Identety
+        rl.question('Enter name and Mnemonic?(leave mnemonic empty for auto generation)', (answer) => {
             var inputs = answer.split(" ")
             var identity = {
                 name: inputs.shift(),
                 mnemonic: inputs.join(" ")
             }
 
-            if (identity.mnemonic === "") {
+            if(identity.name === ""){
+                console.log("Please enter a name")
+            }
+
+            if (identity.mnemonic === "") { // generate if not given
                 identity.mnemonic = Kilt.Identity.generateMnemonic()
             }
 
             try {
+                // try to build identety from mnemonic hance check if mnemonic is valid
                 identity.address = Kilt.Identity.buildFromMnemonic(identity.mnemonic).address
+                // store the new identity
                 var ids = store.get(storageLocation)
                 if (ids == null) {
                     ids = []
@@ -24,21 +30,48 @@ module.exports = {
                 ids.push(identity)
                 store.set(storageLocation, ids)
                 console.log(storageLocation + " created with name '" + identity.name + "' and mnemonic '" + identity.mnemonic + "'")
-
             } catch (e) {
-                console.log(e) //"Invalid Mnemonic make sure it made up of 12 words")
+                console.log("Invalid Mnemonic make sure it is made up of 12 words")
             }
         });
     },
-    removeIdentety: function (rl, storageLocation) {
-
-        rl.question('Enter name : ', (name) => {
+    removeIdentety: function (rl, storageLocation) { // removes the first identity from local storage with the specified name
+        rl.question('Enter identety name : ', (name) => {
             var ids = store.get(storageLocation)
             for (var i = 0; i < ids.length; i++) {
                 if (name === ids[i].name) {
                     console.log(storageLocation + " removed with name '" + ids[i].name + "' and mnemonic '" + ids[i].mnemonic + "'")
                     ids.splice(i, 1)
                     store.set(storageLocation, ids)
+                    return
+                }
+            }
+            console.log("No " + storageLocation + " found under the name of " + name)
+        });
+    },
+    createDid: function (rl, storageLocation) {
+        rl.question('Enter identety name : ', (name) => {
+            var ids = store.get(storageLocation)
+            for (var i = 0; i < ids.length; i++) {
+                if (name === ids[i].name) {
+                    // setup Claimer
+                    const mnemonic = ids[i].mnemonic
+
+                    const identity = Kilt.Identity.buildFromMnemonic(mnemonic)
+                    // create did object
+                    const did = Kilt.Did.fromIdentity(identity)
+
+                    // add to blockchain
+                    Kilt.default.connect('wss://full-nodes.kilt.io:9944')
+                    did.store(identity).then(() =>{
+                        // store did object 
+                        ids[i]["did"] = did.createDefaultDidDocument()
+                        store.set(storageLocation, ids)
+                    }).catch( e => {
+                        console.log(e)
+                    }).finally (() =>{
+                        Kilt.default.disconnect('wss://full-nodes.kilt.io:9944')
+                    })
                     break
                 }
             }
